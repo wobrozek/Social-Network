536e2b8a8525fbc4f432e79e44a19ea31a9d56c6
Wojciech Brożek <wobrozek@gmail.com>
2022-08-02 14:48:43 +0200

    add model

--- project4/network/models.py
+++ project4/network/models.py
@@ -1,6 +1,42 @@
+import email
+from sqlite3 import Timestamp
+from xml.etree.ElementTree import Comment
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+class Post():
+    author = models.ForeignKey("User", on_delete=models.CASCADE, related_name="author")
+    body = models.CharField(max_length=64)
+    likes = models.IntegerField()
+    timestamp=models.DateTimeField(auto_now_add=True)
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "author": self.author,
+            "body": self.body,
+            "likes": self.likes,
+            "timestamp": self.timestamp.strftime("%b %d %Y, %I:%M %p")           
+        }
+
+
+class Comment():
+    author = models.ForeignKey("User", on_delete=models.CASCADE, related_name="author")
+    post= models.ForeignKey("Post",on_delete=models.CASCADE, related_name="author")
+    body = models.CharField(max_length=64)
+    likes = models.IntegerField()
+    timestamp=models.DateTimeField(auto_now_add=True)
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "post":self.post,
+            "author": self.author,
+            "body": self.body,
+            "likes": self.likes,
+            "timestamp": self.timestamp.strftime("%b %d %Y, %I:%M %p")           
+        }
